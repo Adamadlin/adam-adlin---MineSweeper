ceec7f22b74a42a67d984c62d80058abe1b56ce7
Adamadlin <60316094+Adamadlin@users.noreply.github.com>
2021-05-20 19:11:46 +0300

    Add files via upload
    minesweeper 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const MINE = '🧨';
+const EMPTY = ' ';
 var gSize = 4;
 var gscore = 0
 
@@ -12,12 +13,14 @@ var gLevel = {
 
 
 function init() {
-    var gBoard = creatBoard(gSize)
+    var gBoard = buildBoard(gSize)
     renderBoard(gBoard)
+    // renderCell(gBoard[1][1].location, MINE)
+
 }
 
 
-function creatBoard(gSize) {
+function buildBoard(gSize) {
     var board = [];
     for (var i = 0; i < gSize; i++) {
         board[i] = [];
@@ -27,34 +30,74 @@ function creatBoard(gSize) {
                 isShown: false,
                 isMine: false,
                 isMarked: false,
+                // board: MINE,
                 location: { i: i, j: j }
             }
             board[i][j] = cell
 
+            // console.log(cell.location.i)
         }
     }
+
+
+    board[1][2].isMine = true;
+    board[3][1].isMine = true;
+
 
     console.log(board)
     return board;
 }
+// function countFoodAround(mat, rowIdx, colIdx) { // 4 , 0
+
+//     var foodCount = 0;
+//     for (var i = rowIdx - 1; i <= rowIdx + 1; i++) { // 3 4 5
+//         if (i < 0 || i > mat.length - 1) continue;
+//         for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+//             if (j < 0 || j > mat[0].length - 1) continue;
+//             if (i === rowIdx && j === colIdx) continue
+//             var cell = mat[i][j];
+//             // console.log('cell', cell);
+//             if (cell === food) foodCount++
+
+//         }
+//     }
+//     return foodCount
+
+// function countNeighbours(board, rowIdx, colIdx) {
+//     var counterneighbours = 0
+//     for (var j = colIdx - 1; j <= colIdx + 1; j++) continue
+//     if (j < 0 || j > mat[0].length - 1) continue;
+//     if (i === rowIdx && j === colIdx) continue
+//     var cell = board[i][j];
+//     console.log('cell', cell);
+//     if (cell === food) foodCount++
+// }
+
+
 
 function renderBoard(board) {
-    var strHtml = '';
+    var strHtml = '<table><tbody>';
     for (var i = 0; i < board.length; i++) {
         strHtml += '<tr>'
         for (var j = 0; j < board[0].length; j++) {
             var cell = board[i][j];
             var className = 'cell cell-' + i + '-' + j;
-            strHtml += `<td class="${className}" onclick="cellClicked(this ,${cell.location})">  </td>`
+            if (board[i][j].isMine === true) {
+                strHtml += `<td class="${className}" onclick="cellClicked(this ,${cell.location})"> ${MINE} </td>`
+            } else {
+                strHtml += `<td class="${className}" onclick="cellClicked(this ,${cell.location})"> ${EMPTY} </td>`
+            }
         }
         strHtml += '</tr>'
     }
 
-    // renderCell(board[1][1].location, MINE)
+
+
 
     var elTable = document.querySelector('.board')
     elTable.innerHTML = strHtml
 }
+
 
 
 function difficulty(elBtn) {
